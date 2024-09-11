25df8c1aab84ac57c19b5eb43f91ad64226a4a7c
Matheus De O Zacanini <zacanini2@gmail.com>
2024-09-10 21:35:43 -0300

    Upando Repósitorio (UseMemo)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useMemo, useState} from 'react';
+
+const getLista = dados => {
+  console.log(`Entrou em getLista : ${x.id}`)
+  return (
+    <ul>
+      {dados.map(x => <li key={x.id}>{x.texto}</li>)}
+    </ul>
+  );
+};
 
 function App() {
+  const [texto, setTexto] = useState('');
+  const [dados, setDados] = useState([]);
+
+  const lista = useMemo(() => getLista(dados), [dados]) ;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={texto} onChange={e => setTexto(e.target.value)}/>
+      <button onClick={() => setDados([...dados, {id:Number(Date.now()) , texto }])}>+</button>
+      { lista }
     </div>
   );
 }
